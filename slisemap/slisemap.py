fix a scaling bug in fit_new and add verbosity

slisemap/slisemap.py
@@ -799,6 +799,7 @@ class Slisemap:
         between: bool = True,
         escape_fn: Callable = escape_neighbourhood,
         loss: bool = False,
+        verbose: bool = False,
         **kwargs,
     ) -> Tuple[np.ndarray, np.ndarray, Optional[np.ndarray]]:
         """Generate embedding(s) and model(s) for new data item(s).
@@ -814,6 +815,7 @@ class Slisemap:
             between (bool, optional): If ``optimise=True``, should the new points affect each other? Defaults to True.
             escape_fn (Callable, optional): Escape function to use as initialisation (escape_neighbourhood/escape_greedy/escape_marginal). Defaults to escape_neighbourhood.
             loss (bool, optional): Return a vector of individual losses for the new items. Defaults to False.
+            verbose (bool, optional): Print status messages. Defaults to False.
             **kwargs: Optional keyword arguments to LBFGS.
 
         Returns:
@@ -822,6 +824,8 @@ class Slisemap:
         Xnew = self._as_new_X(Xnew)
         n = Xnew.shape[0]
         ynew = self._as_new_Y(ynew, n)
+        if verbose:
+            print("Escaping the new data")
         Bnew, Znew = escape_fn(
             X=Xnew,
             Y=ynew,
@@ -837,8 +841,13 @@ class Slisemap:
             Yold=self.Y,
             jit=self.jit,
         )
+        if verbose:
+            Zrad = torch.sqrt(torch.sum(Znew**2) / n).cpu().detach().item()
+            print("  radius(Z_new) =", Zrad)
 
         if optimise:
+            if verbose:
+                print("Optimising the new data")
             lf, set_new = make_marginal_loss(
                 X=self.X,
                 Y=self.Y,
@@ -867,11 +876,22 @@ class Slisemap:
                     LBFGS(lambda: lf(Bi, Zi), [Bi, Zi], **kwargs)
                     Bnew[j] = Bi.detach()
                     Znew[j] = Zi.detach()
+            if verbose:
+                Zrad = torch.sqrt(torch.sum(Znew**2) / n).cpu().detach().item()
+                print("  radius(Z_new) =", Zrad)
         if self.radius > 0:
-            Zout = Znew * (self.radius / (torch.sum(self._Z**2) + 1e-8))
+            if verbose:
+                print("Normalising the solution")
+            norm = self.radius / (torch.sqrt(torch.sum(self._Z**2) / self.n) + 1e-8)
+            Zout = Znew * norm
+            if verbose:
+                Zrad = torch.sqrt(torch.sum(Zout**2) / n).cpu().detach().item()
+                print("  radius(Z_new) =", Zrad)
         else:
             Zout = Znew
         if loss:
+            if verbose:
+                print("Calculating individual losses")
             lf = self.get_loss_fn(individual=True)
             if between:
                 loss = lf(
@@ -893,6 +913,8 @@ class Slisemap:
                         Z=torch.cat((self.Z, Znew[None, j]), 0),
                     )[-1]
                     loss[j] = l.detach().cpu().item()
+            if verbose:
+                print("  mean(loss) =", loss.mean())
             return _tonp(Bnew), _tonp(Zout), loss
         else:
             return _tonp(Bnew), _tonp(Zout)

tests/test_slisemap.py
@@ -51,7 +51,7 @@ def test_fit_new():
     _, _, l2a = sm.fit_new(
         x2, y2, False, between=True, escape_fn=escape_neighbourhood, loss=True
     )
-    _, _, l2b = sm.fit_new(
+    B2, Z2, l2b = sm.fit_new(
         x2, y2, True, between=False, escape_fn=escape_neighbourhood, loss=True
     )
     _, _, l2c = sm.fit_new(
@@ -73,6 +73,14 @@ def test_fit_new():
     assert np.sum(np.abs(l2d - l2a)) < 1e-4
     assert np.sum(np.abs(l2e - l2b)) < 1e-4
     assert np.sum(np.abs(l2f - l2c)) < 1e-4
+    lf = sm.get_loss_fn(individual=True)
+    l2b_ = lf(
+        X=sm._as_new_X(np.concatenate((X, x2), 0)),
+        Y=sm._as_new_Y(np.concatenate((y, y2), 0)),
+        B=torch.cat((sm.B, torch.as_tensor(B2, **sm.tensorargs)), 0),
+        Z=torch.cat((sm.Z, torch.as_tensor(Z2, **sm.tensorargs)), 0),
+    )[sm.n :]
+    assert np.all(l2b_.cpu().numpy() <= l2b + 1e-2)
 
 
 def test_loss():