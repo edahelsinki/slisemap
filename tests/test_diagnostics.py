cf3a5c101de51f63c82c04cc2ccc5d14552abad6
Anton Björklund <anton.bjorklund@helsinki.fi>
2022-07-05 11:51:05 +0300

    message for assert

--- tests/test_diagnostics.py
+++ tests/test_diagnostics.py
@@ -27,12 +27,14 @@ def test_diagnose():
         assert isinstance(name, str)
         assert isinstance(mask, np.ndarray)
         assert mask.shape[0] == sm.n
-        assert np.mean(mask) < 0.2
+        assert np.mean(mask) < 0.2, f"Too many data items flagged in diagnostic: {name}"
     for name, mask in diagnose(sm, conservative=True).items():
         assert isinstance(name, str)
         assert isinstance(mask, np.ndarray)
         assert mask.shape[0] == sm.n
-        assert np.mean(mask) < 0.2
+        assert (
+            np.mean(mask) < 0.2
+        ), f"Too many data items flagged in (conservative) diagnostic: {name}"
 
 
 def test_underfit():
